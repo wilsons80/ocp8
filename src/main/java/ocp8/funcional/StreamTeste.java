4e14ad862c0a896aac803eb2523d5c926954d11a
Wilson Souza <wilsons80@gmail.com>
2018-07-26 09:56:35 -0300

    Estudo sobre Stream, DoubleBinaryOperator, groupingBy, summing*, reduce

--- src/main/java/ocp8/funcional/StreamTeste.java
+++ src/main/java/ocp8/funcional/StreamTeste.java
@@ -1,12 +1,62 @@
 package ocp8.funcional;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.function.DoubleBinaryOperator;
 import java.util.function.Predicate;
+import static java.util.stream.Collectors.*;
 import java.util.stream.Stream;
 
+
+class Transasao{
+	int id;
+	String tipo;
+	Double valor;
+	
+	public Transasao(int id, String tipo, Double valor) {
+		super();
+		this.id = id;
+		this.tipo = tipo;
+		this.valor = valor;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTipo() {
+		return tipo;
+	}
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	public Double getValor() {
+		return valor;
+	}
+	public void setValor(Double valor) {
+		this.valor = valor;
+	}
+	
+}
 public class StreamTeste {
 
+	
+	public List<Transasao> getTransacoes(){
+		List<Transasao> transacoes = Arrays.asList(
+				new Transasao(1, "A", 1.3),
+				new Transasao(2, "A", 4.6),
+				new Transasao(3, "A", 3.1),
+				new Transasao(4, "B", 5.5),
+				new Transasao(5, "C", 7.9),
+				new Transasao(6, "B", 2.4),
+				new Transasao(7, "C", 5.5));
+		return transacoes;
+	}
+	
 	private static void teste01() {
 		Stream<Double> randon = Stream.generate(Math::random).limit(10);
 		randon.forEach(System.out::println);
@@ -35,8 +85,47 @@ public class StreamTeste {
 		System.out.println(valor.stream().allMatch(lower));
 	}	
 	
+	
+	public static void teste03() {
+		List<Transasao> lista = new StreamTeste().getTransacoes();
+		
+		int qtd = lista.stream().map(t -> t.getId()).reduce(0, Integer::sum);
+		System.out.println("qtd: " + qtd);
+
+		qtd = lista.stream().collect(reducing(0, Transasao::getId, Integer::sum));
+		System.out.println("qtd: " + qtd);
+
+		Double media = lista.stream().collect(averagingDouble(Transasao::getValor));
+		System.out.println("media: " + media);
+
+		Optional<Transasao> max = lista.stream().collect(maxBy( Comparator.comparing(Transasao::getValor)));
+		System.out.println("max: " + max.orElse(null).getValor());
+		
+		Map<String, List<Transasao>> ll = lista.stream().collect(groupingBy(Transasao::getTipo));
+		ll.forEach( (k,v) -> {
+			System.out.println(k);
+			v.stream().map(Transasao::getValor).forEach(System.out::println);
+		});
+
+		
+		Map<String, Double> lll = lista.stream().collect(groupingBy(Transasao::getTipo, summingDouble(Transasao::getValor)));
+		lll.forEach( (k,v) -> System.out.println(k + " - " + v)  );
+
+	}
+	
+	public static void teste04() {
+		List<Transasao> lista = new StreamTeste().getTransacoes();
+		
+		DoubleBinaryOperator total = (a,b) -> a + b;
+		
+		Optional<Double> valor = lista.stream().map(Transasao::getValor).reduce(total::applyAsDouble);
+		System.out.println(valor.get());
+		
+	}
+
+	
 	public static void main(String[] args) {
-		teste02();
+		teste04();
 	}
 
 }
